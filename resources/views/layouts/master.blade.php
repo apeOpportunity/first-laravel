a47b3a4390a14982cb13d0308ecf82fecc381565
apeOpportunity <anita@opportunity-zuerich.ch>
2022-05-30 16:29:49 +0200

    #1: added blablabl

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,6 +7,7 @@
     <!-- hier wird der Wert von der section "title" eines blade templates ausgefüllt, 
     welches dieses layout "extended" -->
 
+    <!-- todo: do this -->
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
