9653e844bd2768844cc8d64c1d2afcd2f52d2557
apeOpportunity <anita@opportunity-zuerich.ch>
2022-06-28 08:59:22 +0200

    added timestamps to messageSeeder

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -18,6 +18,8 @@ class MessageSeeder extends Seeder
         DB::table('messages')->insert([
             'title' => 'Message 1',
             'content' => 'Content for message 1',
+            'created_at' =>  date("Y-m-d H:i:s"), 
+            'updated_at' =>  date("Y-m-d H:i:s")
         ]);        
 
         DB::table('messages')->insert([
